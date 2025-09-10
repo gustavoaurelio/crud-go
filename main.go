d9c9b548abe25f124f28fdff53b2a05fb617b4b8
Gustavo <gustavoaurelio1024@proton.me>
2025-09-09 21:44:24 -0300

    Initializing env file

--- main.go
+++ main.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Erro ao carregar o arquivo .env")
+	}
+	fmt.Println(os.Getenv("TEST"))
 }
