41082259b2367f20486a6ca5d982d954282a5d53
Gustavo <gustavoaurelio1024@proton.me>
2025-09-10 23:37:53 -0300

    Padronization of http errors complete for now

--- src/configuration/rest_err.go
+++ src/configuration/rest_err.go
@@ -1,6 +1,8 @@
 package rest_err
 
-import "net/http"
+import (
+	"net/http"
+)
 
 type RestErr struct {
 	Message string   `json:"message"`
@@ -23,10 +25,49 @@ func NewRestErr(message, err string, code int, causes []Causes) *RestErr {
 	}
 }
 
+func (r *RestErr) Error() string {
+	return r.Message
+}
+
 func NewBadRequestError(message string) *RestErr {
-	return &RestErr{Message: message,
-		Err:  "bad_request",
-		Code: http.StatusBadRequest,
+	return &RestErr{
+		Message: message,
+		Err:     "bad_request",
+		Code:    http.StatusBadRequest,
 	}
 
+}
+
+func NewBadRequestValidationError(message string, causes []Causes) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "bad_request",
+		Code:    http.StatusBadRequest,
+		Causes:  causes,
+	}
+
+}
+
+func NewInternalServerError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "internal_server_error",
+		Code:    http.StatusInternalServerError,
+	}
+}
+
+func NewNotFoundError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "not_found",
+		Code:    http.StatusNotFound,
+	}
+}
+
+func NewForbiddenError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "forbidden",
+		Code:    http.StatusForbidden,
+	}
 }
