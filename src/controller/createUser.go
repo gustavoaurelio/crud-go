b3d52c89d02f6f25cf78335f5e0af8b7ea9b4f59
Gustavo Aurelio <gustavoaurelio1024@proton.me>
2025-09-14 21:43:24 -0300

    Testing error

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,5 +1,12 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
 
-func CreateUser(c *gin.Context) {}
+	rest_err "github.com/gustavoaurelio/go-crud/src/configuration"
+)
+
+func CreateUser(c *gin.Context) {
+	err := rest_err.NewBadRequestError("Rota errada")
+	c.JSON(err.Code, err)
+}
