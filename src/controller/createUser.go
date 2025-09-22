279c8f10b9dc484a64427dc488451616c5eeebab
Gustavo <gustavoaurelio1024@proton.me>
2025-09-22 03:56:42 -0300

    Testing createUser route(bug)

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,12 +1,25 @@
 package controller
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 
 	rest_err "github.com/gustavoaurelio/go-crud/src/configuration"
+	"github.com/gustavoaurelio/go-crud/src/controller/model/request"
 )
 
 func CreateUser(c *gin.Context) {
-	err := rest_err.NewBadRequestError("Rota errada")
-	c.JSON(err.Code, err)
+
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := rest_err.NewBadRequestError(
+			fmt.Sprintf("There are some incorrect fields!, error=%s \n", err.Error))
+
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+
+	fmt.Println(userRequest)
 }
